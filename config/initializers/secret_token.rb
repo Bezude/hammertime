7b8a8ae5864aa673822127d24cd25f13d883dfea
Ben Roye <benjamin.reed.roye@gmail.com>
2013-12-24 17:41:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hammertime::Application.config.secret_key_base = '64fa7dfee683382d164f87ea902cac7283bef27efdec6354b8fc4af00c37339bacaf9ca16b696488f0007118338b784531d211fc87d5e678200f0069f7e71485'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hammertime::Application.config.session_store :cookie_store, key: '_hammertime_session'
